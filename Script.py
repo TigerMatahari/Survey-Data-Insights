58a9a18458c6d41b64a7103eee95fb696b724a4d
Garry Sands <96566126+TigerMatahari@users.noreply.github.com>
2022-01-04 21:56:25 -0800

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,10 @@
+frequency = "once a week"
+intensity = "low"
+
+highly_active = frequency == "daily"
+print("Highly active user:")
+print(highly_active)
+
+high_intensity = intensity == "high"
+print("High intensity activity:")
+print(high_intensity)
